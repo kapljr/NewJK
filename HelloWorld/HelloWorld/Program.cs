1e241ce587e153c84ae9b86cbfd2e37ff5d5bec1
kapljr <winnersall@aol.com>
2016-06-17 22:33:08 -0400

    Jay's Hello World
    sample hello world c# program

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+//namespace is a way to organize the code
+{
+    class Program
+    //class is a container for all of the methods
+    {
+        static void Main(string[] args)
+        {
+            //Main code block
+            //First method called when application is called
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
